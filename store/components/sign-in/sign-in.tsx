54b5536a02395e93bce8efc45d2bd1dd6e1744a2
Yash Raj Maiya <yrmaiya@gmail.com>
2023-12-06 10:43:04 +0530

    Update sign-in component to use Clerk's SignedIn and SignedOut components

--- store/components/sign-in/sign-in.tsx
+++ store/components/sign-in/sign-in.tsx
@@ -1,14 +1,27 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import { SignInButton } from '@clerk/clerk-react';
 import { UserCircle } from 'lucide-react';
+import {
+    ClerkProvider,
+    SignedIn,
+    SignedOut,
+    SignInButton,
+    UserButton,
+} from "@clerk/nextjs";
 
 const LoginPage = () => {
     return (
         <div>
-            <SignInButton>
-                <UserCircle size={20} color="white" />
-            </SignInButton>
+            <SignedIn>
+                {/* Mount the UserButton component */}
+                <UserButton />
+            </SignedIn>
+            <SignedOut>
+                {/* Signed out users get sign in button */}
+                <SignInButton>
+                    <UserCircle size={20} color="white" />
+                </SignInButton>
+            </SignedOut>
         </div>
     );
 };
