09934b59918ce09bf55f4588ad88c2b45196b728
Somesh <someshmohanty08@email.com>
2023-12-07 23:33:36 +0530

    Update API URL and remove unused import

--- store/components/main-nav.tsx
+++ store/components/main-nav.tsx
@@ -6,8 +6,6 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Category } from "@/types";
 
-import React, { useState } from "react";
-
 interface MainNavProps {
   data: Category[];
 }
