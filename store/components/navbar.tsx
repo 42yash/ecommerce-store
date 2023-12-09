63267eb1082776d154ed953467a901853f1c5af2
Somesh <someshmohanty08@email.com>
2023-12-09 15:40:26 +0530

    Fix navbar styles and update button colors

--- store/components/navbar.tsx
+++ store/components/navbar.tsx
@@ -9,11 +9,13 @@ const Navbar = async () => {
   const categories = await getCategories();
 
   return (
-    <div className="border-b border-black bg-purple-300">
+    <div className="border-b border-black bg-purple-700">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">SHAMAN SILVER STORE</p>
+            <p className="font-bold text-xl text-yellow-100">
+              SHAMAN SILVER STORE
+            </p>
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
