b0413cd14c310e4b37068671d7a085727bb4a3da
Somesh <someshmohanty08@email.com>
2023-12-09 17:13:17 +0530

    Add mobile main navigation component

--- store/components/navbar.tsx
+++ store/components/navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 import MainNav from "@/components/main-nav";
+import MobileMainNav from "./mobile-main-nav";
 import Container from "@/components/ui/container";
 import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
@@ -13,11 +14,16 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl text-yellow-100">
+            <p className="font-bold text-sm sm:text-xl text-yellow-100">
               SHAMAN SILVER STORE
             </p>
           </Link>
-          <MainNav data={categories} />
+          <div className="hidden lg:block">
+            <MainNav data={categories} />
+          </div>
+          <div className="lg:hidden">
+            <MobileMainNav data={categories} />
+          </div>
           <NavbarActions />
         </div>
       </Container>
