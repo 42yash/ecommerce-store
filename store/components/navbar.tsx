1c1ea9e33c4b7779fd7e78b5e4923d70b3042a45
Somesh <someshmohanty08@email.com>
2023-12-10 11:16:40 +0530

    Refactor navbar component to improve responsiveness

--- store/components/navbar.tsx
+++ store/components/navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = async () => {
           <div className="hidden lg:block">
             <MainNav data={categories} />
           </div>
-          <div className="lg:hidden flex flex-col">
+          <div className="lg:hidden">
             <MobileMainNav data={categories} />
           </div>
           <NavbarActions />
