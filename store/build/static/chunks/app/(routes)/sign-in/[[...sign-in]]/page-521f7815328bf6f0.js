c5e20555ee1c1423aaf9406271d152eff34caaa3
Yash Raj Maiya <yrmaiya@gmail.com>
2023-12-16 19:16:13 +0530

    Delete unused files and update API URL

--- store/build/static/chunks/app/(routes)/sign-in/[[...sign-in]]/page-521f7815328bf6f0.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[495,372],{5809:function(e,n,t){Promise.resolve().then(t.bind(t,2145)),Promise.resolve().then(t.bind(t,3530)),Promise.resolve().then(t.bind(t,1507)),Promise.resolve().then(t.bind(t,1726)),Promise.resolve().then(t.t.bind(t,9167,23))},4033:function(e,n,t){e.exports=t(94)},1853:function(e,n,t){"use strict";/**
- * @license React
- * use-sync-external-store-shim.production.min.js
- *
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var o=t(2265),u="function"==typeof Object.is?Object.is:function(e,n){return e===n&&(0!==e||1/e==1/n)||e!=e&&n!=n},i=o.useState,s=o.useEffect,c=o.useLayoutEffect,f=o.useDebugValue;function r(e){var n=e.getSnapshot;e=e.value;try{var t=n();return!u(e,t)}catch(e){return!0}}var a="undefined"==typeof window||void 0===window.document||void 0===window.document.createElement?function(e,n){return n()}:function(e,n){var t=n(),o=i({inst:{value:t,getSnapshot:n}}),u=o[0].inst,a=o[1];return c(function(){u.value=t,u.getSnapshot=n,r(u)&&a({inst:u})},[e,t,n]),s(function(){return r(u)&&a({inst:u}),e(function(){r(u)&&a({inst:u})})},[e]),f(t),t};n.useSyncExternalStore=void 0!==o.useSyncExternalStore?o.useSyncExternalStore:a},6272:function(e,n,t){"use strict";e.exports=t(1853)}},function(e){e.O(0,[432,971,472,744],function(){return e(e.s=5809)}),_N_E=e.O()}]);
