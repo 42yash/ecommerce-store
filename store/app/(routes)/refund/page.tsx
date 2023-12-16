83ba219731ab659729ee904d81a2f104df75ee65
Som3sh <someshmohanty08@gmail.com>
2023-12-16 22:37:39 +0530

    Add refund and privacy policy pages

---
+++ store/app/(routes)/refund/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <h1>Refund Policy</h1>
+      <p></p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
--- store/components/footer.tsx
+++ store/components/footer.tsx
@@ -3,6 +3,7 @@ import { FaInstagram } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -10,13 +11,10 @@ const Footer = () => {
       <div className="bg-purple-50 h-1/2 w-full flex md:flex-row flex-col justify-around items-start p-20">
         <div className="p-5 ">
           <ul>
-            <p className="text-gray-500 font-bold text-2xl pb-6">
-              Certificate of Approval
-            </p>
             <div className="flex gap-6 pb-5">
               <img
-                className="h-80 w-19 ml-3"
-                src="https://i.pinimg.com/564x/17/63/ea/1763ea151a1f9dfbbb948c68c895a8f3.jpg"
+                className="h-80 w-15 ml-auto"
+                src="https://i.pinimg.com/originals/5e/79/4a/5e794ae64d84c9220cbbf94f336b7ab0.jpg"
               />
               {/* <FaInstagram className="text-2xl cursor-pointer hover:text-yellow-600" />
               <FaTwitter className="text-2xl cursor-pointer hover:text-blue-600" />
@@ -28,16 +26,16 @@ const Footer = () => {
         <div className="p-5">
           <ul>
             <p className="text-gray-800 font-bold text-xl pb-4">
-              General Information
+              <a href="">General Information</a>
             </p>
             <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-              Privacy Policy
+              <Link href="/privacy">Privacy Policy</Link>
             </li>
             <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-              Terms of Services
+              <Link href="/Terms">Terms & Conditions</Link>
             </li>
             <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-              Return Policy
+              <Link href="/refund">Refund Policy</Link>
             </li>
           </ul>
         </div>
