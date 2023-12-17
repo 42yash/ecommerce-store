1fa502e5b390d3b56c086b192dac5698f14900ca
Som3sh <someshmohanty08@gmail.com>
2023-12-17 12:45:09 +0530

    Add Contact Us page and link in footer

---
+++ store/app/(routes)/Contact/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="items-center text-center">
+      <h1>Contact Us</h1>
+      <p>Email: shamanenterprisesbangalore@gmail.com</p>
+      <p>Mobile No.: 89715 02804</p>
+      <p>Legal Name: Shaman Enterprises</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
